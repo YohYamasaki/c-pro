a0cd83d888da4d6be093ed281e0d6ef47a1e75d9
Yohei <74522538+YohyYamasaki@users.noreply.github.com>
2025-04-01 07:32:33 +0200

    abc217

--- abc217/src/bin/b.rs
+++ abc217/src/bin/b.rs
@@ -1,3 +1,25 @@
+use std::collections::HashSet;
+
 use proconio::*;
 
-fn main() {}
+fn main() {
+    input! {
+        s1: String,
+        s2: String,
+        s3: String
+    }
+
+    let mut st = HashSet::from([
+        String::from("ABC"),
+        String::from("ARC"),
+        String::from("AGC"),
+        String::from("AHC"),
+    ]);
+    st.remove(&s1);
+    st.remove(&s2);
+    st.remove(&s3);
+
+    for i in st {
+        println!("{i}");
+    }
+}
--- abc217/src/bin/c.rs
+++ abc217/src/bin/c.rs
@@ -1,5 +1,17 @@
 use proconio::*;
 
 fn main() {
-    
+    input! {
+        n: i32,
+        p: [i32; n]
+    }
+
+    let mut ans = vec![0; n as usize];
+    for (i, el) in p.iter().enumerate() {
+        ans[(el - 1) as usize] = i + 1;
+    }
+    for i in ans {
+        print!("{i} ");
+    }
+    println!("");
 }
