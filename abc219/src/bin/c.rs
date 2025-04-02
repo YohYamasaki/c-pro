e3632f7764a14958444390725ec1f3274aa1fff7
Yohei <74522538+YohyYamasaki@users.noreply.github.com>
2025-04-02 17:59:33 +0200

    abc219

--- abc219/src/bin/c.rs
+++ abc219/src/bin/c.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use proconio::*;
 
 fn main() {
@@ -7,6 +9,29 @@ fn main() {
         s: [String; n]
     }
 
-    let mut si: Vec<usize> = vec![];
-    for i in s {}
+    let mut char_to_i: HashMap<char, usize> = HashMap::new();
+    let mut i_to_char: HashMap<usize, char> = HashMap::new();
+    for (i, c) in x.chars().enumerate() {
+        char_to_i.insert(c, i);
+        i_to_char.insert(i, c);
+    }
+
+    let mut words_index: Vec<Vec<usize>> = vec![];
+
+    for word in s {
+        let mut si: Vec<usize> = vec![];
+        for c in word.chars() {
+            si.push(*char_to_i.get(&c).unwrap());
+        }
+        words_index.push(si);
+    }
+
+    words_index.sort();
+
+    for w in words_index {
+        for c in w {
+            print!("{}", i_to_char.get(&c).unwrap());
+        }
+        println!("");
+    }
 }
